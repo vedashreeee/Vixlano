087827960e5c3ad72a768c8b181121e6d9c1e8b5
vedashreeee <vedas297@gmail.com>
2025-08-15 11:51:04 +0530

    Fix GitHub Pages deployment with Vite

--- main.jsx
+++ main.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -7,8 +6,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Vixlano">
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
